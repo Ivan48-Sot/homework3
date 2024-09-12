93e24fdae341883ba0635354dce0494a9e96c8b9
Ivan48-Sot <iw4n.sot@yandex.ru>
2024-09-12 21:42:22 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,5 @@
+umber_of_homework = 12
+Number_of_hours = 1.5
+Course_name = 'Python'
+Time_for_one_homework = Number_of_hours / umber_of_homework
+print(Course_name,', всего задач:',umber_of_homework,', затрачено часов:',Number_of_hours,', среднее время выполнения',Time_for_one_homework, 'часа')
